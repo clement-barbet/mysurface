0008ecbe6f6dbed32ea009b9d7a2a5c59263b1aa
Leire Lasa <leire.lasa.amo@gmail.com>
2024-04-05 14:00:31 +0200

    Added possibility to remove participants

--- src/app/api/participants/route.ts
+++ src/app/api/participants/route.ts
@@ -3,40 +3,70 @@ import { NextResponse } from "next/server";
 import { z } from "zod";
 import { cookies } from "next/headers";
 const formSchema = z.object({
-  name: z.string().min(2, {
-    message: "Name must be at least 2 characters.",
-  }),
-  email: z.string().email({
-    message: "Invalid email address.",
-  }),
+	name: z.string().min(2, {
+		message: "Name must be at least 2 characters.",
+	}),
+	email: z.string().email({
+		message: "Invalid email address.",
+	}),
 });
 
 export async function POST(request: Request) {
-  const supabase = createServerComponentClient({ cookies });
+	const supabase = createServerComponentClient({ cookies });
 
-  try {
-    const body = await request.json();
-    const data = formSchema.parse(body);
+	try {
+		const body = await request.json();
+		const data = formSchema.parse(body);
 
-    const { error } = await supabase.from("participants").insert([data]);
+		const { error } = await supabase.from("participants").insert([data]);
 
-    if (error) {
-      throw error;
-    }
+		if (error) {
+			throw error;
+		}
 
-    return NextResponse.json({ message: "Participant created successfully" });
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return NextResponse.json(
-        { error: error.issues },
-        { status: 400, statusText: "Bad Request" }
-      );
-    }
+		return NextResponse.json({
+			message: "Participant created successfully",
+		});
+	} catch (error) {
+		if (error instanceof z.ZodError) {
+			return NextResponse.json(
+				{ error: error.issues },
+				{ status: 400, statusText: "Bad Request" }
+			);
+		}
 
-    console.error("Error creating participant:", error);
-    return NextResponse.json(
-      { error: "An error occurred while creating the participant" },
-      { status: 500, statusText: "Internal Server Error" }
-    );
-  }
+		console.error("Error creating participant:", error);
+		return NextResponse.json(
+			{ error: "An error occurred while creating the participant" },
+			{ status: 500, statusText: "Internal Server Error" }
+		);
+	}
+}
+
+export async function DELETE(request: Request) {
+	const supabase = createServerComponentClient({ cookies });
+
+	try {
+		const body = await request.json();
+		const { id } = body;
+
+		const { error } = await supabase
+			.from("participants")
+			.delete()
+			.eq("id", id);
+
+		if (error) {
+			throw error;
+		}
+
+		return NextResponse.json({
+			message: "Participant deleted successfully",
+		});
+	} catch (error) {
+		console.error("Error deleting participant:", error);
+		return NextResponse.json(
+			{ error: "An error occurred while deleting the participant" },
+			{ status: 500, statusText: "Internal Server Error" }
+		);
+	}
 }
--- src/app/home/participants/columns.tsx
+++ src/app/home/participants/columns.tsx
@@ -4,77 +4,115 @@ import Link from "next/link";
 import { z } from "zod";
 
 const participantsSchema = z.object({
-  name: z.string().min(2).max(50),
-  email: z.string().email(),
-  questionnaire: z.union([z.string().uuid(), z.null()]),
+	id: z.string().uuid(),
+	name: z.string().min(2).max(50),
+	email: z.string().email(),
+	questionnaire: z.union([z.string().uuid(), z.null()]),
+	delete: z.string(),
 });
 
 export type Participants = z.infer<typeof participantsSchema>;
 
 export const columns = (questionnaires: any[]): ColumnDef<Participants>[] => [
-  {
-    accessorKey: "name",
-    header: "Name",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    id: "status",
-    header: "Status",
-    cell: ({ row }) => {
-      const questionnaire = row.original.questionnaire;
-      if (questionnaire === null) {
-        return (
-          <div className="bg-gray-300 text-white px-2 py-1 rounded">
-            Undefined
-          </div>
-        );
-      }
-      const linkedQuestionnaire = questionnaires.find(
-        (q) => q.id === questionnaire
-      );
-      if (linkedQuestionnaire) {
-        if (linkedQuestionnaire.completed) {
-          return (
-            <div className="bg-green-500 text-white px-2 py-1 rounded">
-              Completed
-            </div>
-          );
-        } else {
-          return (
-            <div className="bg-yellow-500 text-white px-2 py-1 rounded">
-              To Complete
-            </div>
-          );
-        }
-      }
-      return (
-        <div className="bg-gray-300 text-white px-2 py-1 rounded">
-          Undefined
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "questionnaire",
-    header: "Questionnaire",
-    cell: ({ row }) => {
-      const questionnaireId = row.original.questionnaire;
-      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-      return questionnaireId ? (
-        <Button
-          onClick={() =>
-            navigator.clipboard.writeText(
-              `${baseUrl}/questionnaire/${questionnaireId}`
-            )
-          }
-          className=" bg-blue-500 px-2 py-1 rounded text-white"
-        >
-          Copy Link
-        </Button>
-      ) : null;
-    },
-  },
+	{
+		accessorKey: "id",
+		header: "ID",
+	},
+	{
+		accessorKey: "name",
+		header: "Name",
+	},
+	{
+		accessorKey: "email",
+		header: "Email",
+	},
+	{
+		id: "status",
+		header: "Status",
+		cell: ({ row }) => {
+			const questionnaire = row.original.questionnaire;
+			if (questionnaire === null) {
+				return (
+					<div className="bg-gray-300 text-white px-2 py-1 rounded">
+						Undefined
+					</div>
+				);
+			}
+			const linkedQuestionnaire = questionnaires.find(
+				(q) => q.id === questionnaire
+			);
+			if (linkedQuestionnaire) {
+				if (linkedQuestionnaire.completed) {
+					return (
+						<div className="bg-green-500 text-white px-2 py-1 rounded">
+							Completed
+						</div>
+					);
+				} else {
+					return (
+						<div className="bg-yellow-500 text-white px-2 py-1 rounded">
+							To Complete
+						</div>
+					);
+				}
+			}
+			return (
+				<div className="bg-gray-300 text-white px-2 py-1 rounded">
+					Undefined
+				</div>
+			);
+		},
+	},
+	{
+		accessorKey: "questionnaire",
+		header: "Questionnaire",
+		cell: ({ row }) => {
+			const questionnaireId = row.original.questionnaire;
+			const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+			return questionnaireId ? (
+				<Button
+					onClick={() =>
+						navigator.clipboard.writeText(
+							`${baseUrl}/questionnaire/${questionnaireId}`
+						)
+					}
+					className=" bg-blue-500 px-2 py-1 rounded text-white"
+				>
+					Copy Link
+				</Button>
+			) : null;
+		},
+	},
+	{
+		accessorKey: "delete",
+		header: "Delete",
+		cell: ({ row }) => {
+			const participantId = row.original.id;
+			return participantId ? (
+				<Button
+					onClick={async () => {
+						const response = await fetch("/api/participants", {
+							method: "DELETE",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({ id: participantId }),
+						});
+
+						if (response.ok) {
+							location.reload();
+						} else {
+							console.error(
+								"Error deleting participant:",
+								response.statusText
+							);
+						}
+					}}
+					className="bg-red-500 px-2 py-1 rounded text-white"
+				>
+					Delete
+				</Button>
+			) : null;
+		},
+	},
 ];
