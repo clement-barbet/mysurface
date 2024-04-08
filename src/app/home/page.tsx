180ee2317ddb592b094866bea5758c5231942d2e
Leire Lasa <leire.lasa.amo@gmail.com>
2024-04-08 15:10:55 +0200

    Added home layout and content

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,130 @@
-export default function Dashboard() {
+"use client";
+
+import { Notification } from "@/components/home/notification";
+
+export default function Home() {
 	return (
-        <h2>Home</h2>
+		<div className="flex flex-col gap-y-4">
+			<div className="flex flex-col md:flex-row md:gap-x-4 gap-y-4">
+				<div className="h-auto w-full flex flex-col shadow-md rounded-lg bg-white dark:bg-black bg-opacity-90">
+					<div className="px-10 py-5">
+						<h2 className="font-bold text-lg">
+							Admin&apos;s Guide
+						</h2>
+						<p className="text-gray-600 dark:text-gray-400">
+							Steps to follow for admin tasks.
+						</p>
+						<ol className="text-gray-600 dark:text-gray-400 list-inside py-4">
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									1.
+								</span>
+								Log-in with an administrator account
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									2.
+								</span>
+								Go to "Participants" section
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									3.
+								</span>
+								Inside "Participants" create new participants
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									4.
+								</span>
+								Click "Create Questionnaire" button to create
+								questionnaires for the participants
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									5.
+								</span>
+								Copy each participant link and share with them
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									6.
+								</span>
+								Make participants complete the questionnaire
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									7.
+								</span>
+								Go to "Results" page
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									8.
+								</span>
+								Click "Generate report" button and the animation
+								with analytics will show up
+							</li>
+						</ol>
+					</div>
+				</div>
+				<div className="h-auto w-full flex flex-col shadow-md rounded-lg bg-white dark:bg-black bg-opacity-90">
+					<div className="px-10 py-5">
+						<h2 className="font-bold text-lg">User&apos;s Guide</h2>
+						<p className="text-gray-600 dark:text-gray-400">
+							Steps to follow for user tasks.
+						</p>
+						<ol className="text-gray-600 dark:text-gray-400 list-inside py-4">
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									1.
+								</span>
+								Access the link provided by your administrator
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									2.
+								</span>
+								Access the questionnaire shared by your
+								administrator
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									3.
+								</span>
+								Complete the questionnaire questions about your
+								teammates / classmates
+							</li>
+							<li className="text-sm">
+								<span className="text-xl font-bold text-blue-500 inline-block w-6">
+									4.
+								</span>
+								Send the results to your administrator
+							</li>
+						</ol>
+					</div>
+				</div>
+			</div>
+			<div className="h-auto w-full flex flex-col shadow-md rounded-lg bg-white dark:bg-black bg-opacity-90">
+				<div className="px-10 py-5">
+					<div className="mb-2">
+						<h2 className="font-bold text-lg">Updates</h2>
+						<p className="text-gray-600 dark:text-gray-400">
+							Check platform updates and news.
+						</p>
+					</div>
+					<Notification
+						type="update"
+						msg="New version of MySurface available."
+						link="#"
+					/>
+					<Notification
+						type="news"
+						msg="New functionalities will be added soon."
+						link="#"
+					/>
+				</div>
+			</div>
+		</div>
 	);
 }
---
+++ src/components/home/notification.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { GrUpdate, GrAdd } from "react-icons/gr";
+import { FaTimes } from 'react-icons/fa';
+
+const Notification = ({ type, msg, link }) => {
+	const [isVisible, setIsVisible] = useState(true);
+
+	const notificationTypes = {
+		update: {
+			icon: <GrUpdate className="w-5 h-5 text-white" />,
+			title: 'Update',
+			message: `${msg}`,
+			gradient: 'from-blue-200 to-indigo-500'
+		},
+		news: {
+			icon: <GrAdd className="w-5 h-5 text-white" />,
+			title: 'News',
+			message: `${msg}`,
+			gradient: 'from-lime-300 to-green-500'
+		},
+	};
+
+	const { icon, title, message, gradient } = notificationTypes[type];
+
+	if (!isVisible) {
+		return null;
+	}
+
+	return (
+		<div className="w-full py-1 border-b-2 flex flex-row items-center gap-x-4 hover:bg-light_gray dark:hover:bg-gray-600 dark:hover:bg-opacity-70 rounded-sm px-2 mb-2">
+			<div className={`border-2 border-gray-200 shadow-sm dark:opacity-80 rounded-full min-w-10 min-h-10 flex items-center justify-center bg-gradient-to-br ${gradient}`}>
+				{icon}
+			</div>
+			<div className='md:text-base text-sm'>
+				<h3 className="font-semibold">{title}</h3>
+				<p>{message} <i>(Check <a className='text-blue-500 font-semibold' href={link}>HERE</a>)</i></p>
+			</div>
+			<button 
+				onClick={() => setIsVisible(false)} 
+				className="ml-auto"
+			>
+				<FaTimes />
+			</button>
+		</div>
+	);
+};
+
+export {Notification};
