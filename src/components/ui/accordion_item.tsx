e00c04bda435099fe39eb124539c212157c0abf1
Leire Lasa <leire.lasa.amo@gmail.com>
2024-04-10 10:01:57 +0200

    Updated styles for accordion

--- src/components/ui/accordion_item.tsx
+++ src/components/ui/accordion_item.tsx
@@ -9,9 +9,9 @@ export const CustomAccordionItem = ({ title, children }) => {
 	return (
 		<AccordionItem className="dark:text-light_gray text-black my-3 bg-sky-100 bg-opacity-70 dark:bg-opacity-20 rounded-md p-5 border border-sky-200 drop-shadow">
 			<AccordionItemHeading>
-				<AccordionItemButton className="text-xl font-semibold">{title}</AccordionItemButton>
+				<AccordionItemButton className="text-lg font-semibold">{title}</AccordionItemButton>
 			</AccordionItemHeading>
-			<AccordionItemPanel className="py-2 px-6 text-lg">{children}</AccordionItemPanel>
+			<AccordionItemPanel className="py-2 md:px-6">{children}</AccordionItemPanel>
 		</AccordionItem>
 	);
 };
