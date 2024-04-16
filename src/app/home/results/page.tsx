97fdb5b2dc77d643f69eed4522cb83a6e71bb534
Leire Lasa <leire.lasa.amo@gmail.com>
2024-04-16 16:46:37 +0200

    Updated /results

--- src/app/home/results/page.tsx
+++ src/app/home/results/page.tsx
@@ -5,46 +5,60 @@ import T from "@/components/translations/translation";
 import Link from "next/link";
 
 export default function Results() {
+	const [loading, setLoading] = useState(true);
 	const [results, setResults] = useState([]);
 	const supabase = createClientComponentClient();
 
 	useEffect(() => {
-		const fetchResults = async () => {
-			let { data, error } = await supabase
+		const fetchData = async () => {
+			setLoading(true);
+			let { data: fetchedResults, error: resultsError } = await supabase
 				.from("results")
 				.select("*")
 				.order("created_at", { ascending: false });
 
-			if (error) console.error("Error loading results", error);
-			else setResults(data);
+			if (resultsError)
+				console.error("Error loading results", resultsError);
+			else setResults(fetchedResults || []);
+			setLoading(false);
 		};
 
-		fetchResults();
+		fetchData();
 	}, []);
 
 	return (
-		<div className="w-full m-auto p-5">
-			<h2 className="text-3xl pb-5">
-				<T tkey="homeresults.title" />
-			</h2>
-			<div className="w-full m-auto p-5 shadow-md rounded-lg bg-white dark:bg-black bg-opacity-90">
-				<ul>
-					{results.map((result) => (
-						<li className="my-2" key={result.id}>
-							<Link
-								href={`/home/results/${result.id}`}
-								className="hover:font-semibold transition-all duration-200 ease-linear"
-							>
-								{`${result.report_name} (${result.id}) - ${
-									new Date(result.created_at)
-										.toISOString()
-										.split("T")[0]
-								}`}
-							</Link>
-						</li>
-					))}
-				</ul>
+		!loading && (
+			<div className="w-full m-auto p-5">
+				<h2 className="text-3xl pb-5">
+					<T tkey="homeresults.title" />
+				</h2>
+				<div className="w-full m-auto p-5 shadow-md rounded-lg bg-white dark:bg-black bg-opacity-90">
+					{results.length > 0 ? (
+						<ul>
+							{results.map((result) => {
+								const date = new Date(result.created_at);
+								const formattedDate =
+									date.toLocaleDateString("en-CA");
+
+								return (
+									<li className="my-2" key={result.id}>
+										<Link
+											href={`/home/results/${result.id}`}
+											className="hover:font-semibold transition-all duration-200 ease-linear"
+										>
+											{`${result.report_name} (${result.id}) - ${formattedDate}`}
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
+					) : (
+						<p>
+							<T tkey="results.nodata" />
+						</p>
+					)}
+				</div>
 			</div>
-		</div>
+		)
 	);
 }
