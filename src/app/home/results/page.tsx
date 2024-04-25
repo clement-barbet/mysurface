08766def061e13aae05d9930a65b09ace72afb42
Leire Lasa <leire.lasa.amo@gmail.com>
2024-04-25 02:35:00 +0200

    Add option to rename report

--- src/app/home/results/page.tsx
+++ src/app/home/results/page.tsx
@@ -13,11 +13,45 @@ import {
 	TBodyRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { Modal, Box, Typography, TextField } from "@mui/material";
+import { FaTimes } from 'react-icons/fa';
 
 export default function Results() {
 	const [loading, setLoading] = useState(true);
 	const [results, setResults] = useState([]);
 	const supabase = createClientComponentClient();
+	const [selectedResult, setSelectedResult] = useState(null);
+	const [open, setOpen] = useState(false);
+	const [reportName, setReportName] = useState("");
+
+	const handleOpen = (result) => {
+		setSelectedResult(result);
+		setReportName(result.report_name);
+		setOpen(true);
+	};
+	const handleSave = async () => {
+		const { error } = await supabase
+			.from("results")
+			.update({ report_name: reportName })
+			.eq("id", selectedResult.id);
+
+		if (error) console.error("Error updating report name", error);
+		else {
+			setResults(
+				results.map((result) =>
+					result.id === selectedResult.id
+						? { ...result, report_name: reportName }
+						: result
+				)
+			);
+		}
+
+		setOpen(false);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	}
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -47,68 +81,141 @@ export default function Results() {
 
 	return (
 		!loading && (
-			<div className="w-full m-auto">
-				<h2 className="text-3xl pb-2">
-					<T tkey="homeresults.title" />
-				</h2>
-				{results.length > 0 ? (
-					<Table className="w-full">
-						<TableHeader>
-							<THeadRow>
-								{["ID", "Name", "Date", "Delete report"].map(
-									(header, index) => (
+			<>
+				<div className="w-full m-auto">
+					<h2 className="text-3xl pb-2">
+						<T tkey="homeresults.title" />
+					</h2>
+					{results.length > 0 ? (
+						<Table className="w-full">
+							<TableHeader>
+								<THeadRow>
+									{[
+										"ID",
+										"Name",
+										"Date",
+										"Delete report",
+										"Edit report's name",
+									].map((header, index) => (
 										<TableHead key={index}>
 											{header}
 										</TableHead>
-									)
-								)}
-							</THeadRow>
-						</TableHeader>
-						<TableBody className="bg-white divide-y divide-gray-200">
-							{results.map((result) => {
-								const date = new Date(result.created_at);
-								const formattedDate =
-									date.toLocaleDateString("en-CA");
-								const formattedTime =
-									date.toLocaleTimeString("en-CA");
+									))}
+								</THeadRow>
+							</TableHeader>
+							<TableBody className="bg-white divide-y divide-gray-200">
+								{results.map((result) => {
+									const date = new Date(result.created_at);
+									const formattedDate =
+										date.toLocaleDateString("en-CA");
+									const formattedTime =
+										date.toLocaleTimeString("en-CA");
 
-								return (
-									<TBodyRow key={result.id}>
-										<TableCell className="px-6 py-4 whitespace-nowrap">
-											<Link
-												href={`/home/results/${result.id}`}
-												className="font-semibold text-blue-500 hover:text-blue-800 underline hover:underline-offset-4 underline-offset-2 transition-all duration-200 ease-linear"
-											>
-												{result.id}
-											</Link>
-										</TableCell>
-										<TableCell className="px-6 py-4 whitespace-nowrap">
-											{result.report_name}
-										</TableCell>
-										<TableCell className="px-6 py-4 whitespace-nowrap">
-											{formattedDate}
-											<br />
-											{formattedTime}
-										</TableCell>
-										<TableCell className="px-6 py-4 whitespace-nowrap">
-											<Button
-												variant="delete"
-												onClick={() =>
-													deleteReport(result.id)
-												}
-											>
-												Delete
-											</Button>
-										</TableCell>
-									</TBodyRow>
-								);
-							})}
-						</TableBody>
-					</Table>
-				) : (
-					<p>No data</p>
-				)}
-			</div>
+									return (
+										<TBodyRow key={result.id}>
+											<TableCell className="px-6 py-4 whitespace-nowrap">
+												<Link
+													href={`/home/results/${result.id}`}
+													className="font-semibold text-blue-500 hover:text-blue-800 underline hover:underline-offset-4 underline-offset-2 transition-all duration-200 ease-linear"
+												>
+													{result.id}
+												</Link>
+											</TableCell>
+											<TableCell className="px-6 py-4 whitespace-nowrap">
+												{result.report_name}
+											</TableCell>
+											<TableCell className="px-6 py-4 whitespace-nowrap">
+												{formattedDate}
+												<br />
+												{formattedTime}
+											</TableCell>
+											<TableCell className="px-6 py-4 whitespace-nowrap">
+												<Button
+													variant="delete"
+													onClick={() =>
+														deleteReport(result.id)
+													}
+												>
+													Delete
+												</Button>
+											</TableCell>
+											<TableCell className="px-6 py-4 whitespace-nowrap">
+												<Button
+													onClick={() =>
+														handleOpen(result)
+													}
+												>
+													Edit
+												</Button>
+											</TableCell>
+										</TBodyRow>
+									);
+								})}
+							</TableBody>
+						</Table>
+					) : (
+						<p>No data</p>
+					)}
+				</div>
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+					BackdropProps={{
+						onClick: (event) => {
+							event.stopPropagation();
+						}
+					}}
+				>
+					<Box
+						sx={{
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							transform: "translate(-50%, -50%)",
+							width: 400,
+							bgcolor: "background.paper",
+							borderRadius: "10px",
+							boxShadow: 24,
+							p: 4,
+						}}
+					>
+						<Button
+							onClick={handleClose}
+							className="absolute top-1 right-1"
+							variant={"delete"}
+						>
+							<FaTimes className="w-3 h-3"/>
+						</Button>
+						<Typography
+							id="modal-modal-title"
+							variant="h6"
+							component="h2"
+							sx={{ mb: 2 }}
+						>
+							Update Report's Name
+						</Typography>
+						<TextField
+							autoFocus
+							margin="dense"
+							id="name"
+							label="Name"
+							type="text"
+							fullWidth
+							value={reportName}
+							onChange={(e) => setReportName(e.target.value)}
+						/>
+						<Button
+							onClick={handleSave}
+							variant="login"
+							className="mt-2"
+						>
+							SAVE
+						</Button>
+					</Box>
+				</Modal>
+			</>
 		)
 	);
 }
