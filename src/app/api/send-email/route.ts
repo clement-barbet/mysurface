4d1bfc37f1b6605754ea11711e46825906d3a2c5
Leire Lasa <leire.lasa.amo@gmail.com>
2024-05-11 16:56:06 +0200

    Updated promises in email route

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -18,6 +18,19 @@ export async function POST(request: Request) {
 	const body = await request.json();
 	const { name, email, url } = body;
 
+	await new Promise((resolve, reject) => {
+		// verify connection configuration
+		transporter.verify(function (error, success) {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				console.log("Server is ready to take our messages");
+				resolve(success);
+			}
+		});
+	});
+
 	let mailOptions = {
 		from: "My Surface® <info@myaudit.org>",
 		to: email,
@@ -26,8 +39,18 @@ export async function POST(request: Request) {
 	};
 
 	try {
-		let info = await transporter.sendMail(mailOptions);
-		console.log("Correo enviado: " + info.response);
+		await new Promise((resolve, reject) => {
+			// send mail
+			transporter.sendMail(mailOptions, (err, info) => {
+				if (err) {
+					console.error(err);
+					reject(err);
+				} else {
+					console.log(info);
+					resolve(info);
+				}
+			});
+		});
 
 		return NextResponse.json({
 			message: "Email sent successfully!",
