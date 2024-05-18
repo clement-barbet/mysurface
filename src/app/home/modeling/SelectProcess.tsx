8a65b15af180500ddf5a2df6b2a5ffc1e8cb89ee
Leire Lasa <leire.lasa.amo@gmail.com>
2024-05-18 20:48:40 +0200

    Updated translations

--- src/app/home/modeling/SelectProcess.tsx
+++ src/app/home/modeling/SelectProcess.tsx
@@ -56,7 +56,7 @@ export default function SelectProcess({ onProcessIdChange }) {
 		<>
 			<div className="p-5 shadow-md rounded-lg bg-white dark:bg-black bg-opacity-90">
 				<h2 className="font-semibold text-lg mb-2  border-l-4 border-mid_blue pl-2">
-					<T tkey="participants.titles.set-process" />
+					<T tkey="participants.titles.set-process.title" />
 				</h2>
 				<div>
 					{selectedProcess == 1 && (
