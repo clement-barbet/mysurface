d187b5ef2e52ed0652c946b29bf99957577ffca3
Leire Lasa <leire.lasa.amo@gmail.com>
2024-05-21 22:39:03 +0200

    Updated head element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,6 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
