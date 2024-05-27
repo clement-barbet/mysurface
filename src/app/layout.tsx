ea2d4629e035ed68067e29a9f2808af5ddb87268
Leire Lasa <leire.lasa.amo@gmail.com>
2024-05-27 18:51:03 +0200

    Update meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,24 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<Head>
+				<meta
+					property="og:url"
+					content={baseUrl ? baseUrl : "https://app.myaudit.org"}
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="MySurface®" />
+				<meta
+					property="og:description"
+					content="App to discover the most influential people or products."
+				/>
+				<meta
+					property="og:image"
+					content={
+						baseUrl
+							? `${baseUrl}/android-chrome-192x192.png`
+							: "https://app.myaudit.org/android-chrome-192x192.png"
+					}
+				/>
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
@@ -44,20 +62,6 @@ export default function RootLayout({
 				/>
 				<meta name="msapplication-TileColor" content="#da532c" />
 				<meta name="theme-color" content="#ffffff" />
-				<meta
-					property="og:url"
-					content={baseUrl ? baseUrl : "https://app.myaudit.org"}
-				/>
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="MySurface®" />
-				<meta
-					property="og:description"
-					content="App to discover the most influential people or products."
-				/>
-				<meta
-					property="og:image"
-					content={(baseUrl ? `${baseUrl}/android-chrome-192x192.png` : "https://app.myaudit.org/android-chrome-192x192.png")}
-				/>
 			</Head>
 			<body
 				className={`${glory.className} bg-dark_gray dark:bg-dark_blue dark:bg-opacity-80 transition-colors duration-1000 linear`}
