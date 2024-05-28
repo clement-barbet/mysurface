256118604dede4cfd96bc3f682eee97a61ef3046
Leire Lasa <leire.lasa.amo@gmail.com>
2024-05-28 18:31:15 +0200

    Handled all cases in license section

--- src/app/home/license/ManageLicenseDev.tsx
+++ src/app/home/license/ManageLicenseDev.tsx
@@ -137,6 +137,137 @@ export default function ManageLicenseDev({ billing, user }) {
 		}
 	};
 
+	const renderActiveSubscriptionType = () => {
+		if (billing.status === "active") {
+			if (billing.subscription === "trial") {
+				return (
+					<span className="lowercase font-semibold underline underline-offset-4">
+						<T tkey="license.details.type.options.trial" />
+					</span>
+				);
+			} else if (billing.subscription === "yearly") {
+				return (
+					<span className="lowercase font-semibold underline underline-offset-4">
+						<T tkey="license.details.type.options.yearly" />
+					</span>
+				);
+			} else {
+				return (
+					<span className="lowercase font-semibold underline underline-offset-4">
+						<T tkey="license.details.type.options.lifetime" />
+					</span>
+				);
+			}
+		}
+	};
+
+	const isExpiringSoon = () => {
+		const today = new Date();
+		const expirationDate = new Date(billing.expiration_date);
+		const diffTime = Math.abs(expirationDate - today);
+		const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+		return diffDays <= 30;
+	};
+
+	console.log(isExpiringSoon());
+
+	const renderActiveSubscription = () => {
+		if (billing.status === "active") {
+			return (
+				<>
+					<p>
+						<T tkey="license.manage.active.a1" />{" "}
+						{renderActiveSubscriptionType()}{" "}
+						<T tkey="license.manage.active.a2" />{" "}
+						<span className="font-semibold uppercase text-accent_color px-1">
+							<T tkey="license.manage.active.a3" />
+						</span>
+						. <T tkey="license.manage.active.a4" />
+					</p>
+					{billing.subscription === "trial" ||
+					(billing.subscription === "yearly" && isExpiringSoon()) ? (
+						<div className="mt-2">
+							<p>
+								{billing.subscription === "trial" ? (
+									<T tkey="license.manage.purchase.p2" />
+								) : (
+									<T tkey="license.manage.purchase.p3" />
+								)}{" "}
+								<span className="font-semibold uppercase text-mid_blue px-1">
+									<T tkey="license.manage.purchase.p4" />
+								</span>{" "}
+								<T tkey="license.manage.purchase.p5" />.
+							</p>
+							<div className="flex justify-end">
+								<Button
+									variant="signup"
+									className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
+									onClick={handleCheckout}
+								>
+									<T tkey="license.manage.buttons.checkout" />
+								</Button>
+							</div>
+						</div>
+					) : null}
+				</>
+			);
+		}
+	};
+
+	const renderInactiveSubscription = () => {
+		if (billing.status === "inactive") {
+			return (
+				<>
+					<div>
+						<p>
+							<T tkey="license.manage.inactive.i1" />{" "}
+							<span className="font-semibold uppercase text-accent_delete px-1">
+								<T tkey="license.manage.inactive.i2" />
+							</span>
+							.
+						</p>
+					</div>
+					{billing.subscription === "none" && !billing.isTrialUsed ? (
+						<>
+							<p className="mt-2">
+								<T tkey="license.manage.trial.t1" />{" "}
+								<span className="font-semibold uppercase text-accent_color px-1">
+									<T tkey="license.manage.trial.t2" />
+								</span>{" "}
+								<T tkey="license.manage.trial.t3" />.
+							</p>
+							<div className="flex justify-end">
+								<Button
+									variant="login"
+									className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
+									onClick={handleNewTrial}
+								>
+									<T tkey="license.manage.buttons.trial" />
+								</Button>
+							</div>
+						</>
+					) : null}
+					<p>
+						<T tkey="license.manage.purchase.p1" />{" "}
+						<span className="font-semibold uppercase text-mid_blue px-1">
+							<T tkey="license.manage.purchase.p4" />
+						</span>{" "}
+						<T tkey="license.manage.purchase.p5" />.
+					</p>
+					<div className="flex justify-end">
+						<Button
+							variant="signup"
+							className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
+							onClick={handleCheckout}
+						>
+							<T tkey="license.manage.buttons.checkout" />
+						</Button>
+					</div>
+				</>
+			);
+		}
+	};
+
 	return (
 		<>
 			<ErrorMessage
@@ -148,69 +279,8 @@ export default function ManageLicenseDev({ billing, user }) {
 				setSuccessMessage={setSuccessMessage}
 			/>
 			<div className="text-sm m-2">
-				{billing.status === "active" ? (
-					<p>
-						<T tkey="license.manage.active.a1" />{" "}
-						<span className="font-semibold uppercase text-accent_color px-1">
-							<T tkey="license.manage.active.a2" />.
-						</span>
-						<T tkey="license.manage.active.a3" />
-					</p>
-				) : billing.subscription === "none" && !billing.isTrialUsed ? (
-					<div>
-						<p>
-							<T tkey="license.manage.trial.t1" />{" "}
-							<span className="font-semibold uppercase text-accent_color px-1">
-								<T tkey="license.manage.trial.t2" />
-							</span>{" "}
-							<T tkey="license.manage.trial.t3" />.
-						</p>
-						<div className="flex justify-end">
-							<Button
-								variant="login"
-								className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
-								onClick={handleNewTrial}
-							>
-								<T tkey="license.manage.buttons.trial" />
-							</Button>
-						</div>
-						<p>
-							<T tkey="license.manage.purchase.p1" />{" "}
-							<span className="font-semibold uppercase text-mid_blue px-1">
-								<T tkey="license.manage.purchase.p2" />
-							</span>{" "}
-							<T tkey="license.manage.purchase.p3" />.
-						</p>
-						<div className="flex justify-end">
-							<Button
-								variant="signup"
-								className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
-								onClick={handleCheckout}
-							>
-								<T tkey="license.manage.buttons.checkout" />
-							</Button>
-						</div>
-					</div>
-				) : (
-					<div>
-						<p>
-							<T tkey="license.manage.inactive.i1" />{" "}
-							<span className="font-semibold uppercase text-accent_delete px-1">
-								<T tkey="license.manage.inactive.i2" />
-							</span>
-							. <T tkey="license.manage.inactive.i3" />.
-						</p>
-						<div className="flex justify-end">
-							<Button
-								variant="signup"
-								className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
-								onClick={handleCheckout}
-							>
-								<T tkey="license.manage.buttons.checkout" />
-							</Button>
-						</div>
-					</div>
-				)}
+				{renderActiveSubscription()}
+				{renderInactiveSubscription()}
 			</div>
 		</>
 	);
