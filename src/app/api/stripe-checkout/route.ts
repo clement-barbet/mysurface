ab8a80f7b7de5e49e2fa93aec825337962f420d6
Leire Lasa <leire.lasa.amo@gmail.com>
2024-05-29 09:44:45 +0200

    Updated non dev stripe routes and components

--- src/app/api/stripe-checkout/route.ts
+++ src/app/api/stripe-checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 const secret_key = process.env.STRIPE_SECRET_KEY;
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-const returnUrlSubs = `${baseUrl}/home/subscription`;
+const returnUrlSubs = `${baseUrl}/home/license`;
 const returnUrlSuccess = `${returnUrlSubs}?session_id={CHECKOUT_SESSION_ID}`;
 const stripe = require("stripe")(secret_key);
 
@@ -11,7 +11,7 @@ export async function POST() {
 	const session = await stripe.checkout.sessions.create({
 		payment_method_types: ["card"],
 		line_items: [{ price: price_id, quantity: 1 }],
-		mode: "subscription",
+		mode: "payment",
 		success_url: returnUrlSuccess,
 		cancel_url: returnUrlSubs,
 	});
--- src/app/home/license/ManageLicense.tsx
+++ src/app/home/license/ManageLicense.tsx
@@ -4,11 +4,10 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
 import { ErrorMessage } from "@/components/ui/msg/error_msg";
 import { SuccessMessage } from "@/components/ui/msg/success_msg";
-import { fetchBilling } from "@/db/billings/fetchBillingByUserId";
-import { set } from "zod";
 import { useRouter } from "next/navigation";
+import T from "@/components/translations/translation";
 
-export default function ManageLicense({ billing, setBilling, user }) {
+export default function ManageLicense({ billing, user }) {
 	const public_key = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
 	const stripePromise = loadStripe(public_key);
 	const supabase = createClientComponentClient();
@@ -21,16 +20,18 @@ export default function ManageLicense({ billing, setBilling, user }) {
 			try {
 				const urlParams = new URLSearchParams(window.location.search);
 				const sessionId = urlParams.get("session_id");
-				console.log("Session ID:", sessionId);
 
 				if (sessionId) {
-					const response = await fetch(`/api/stripe-get-session`, {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json",
-						},
-						body: JSON.stringify({ session_id: sessionId }),
-					});
+					const response = await fetch(
+						`/api/stripe-get-session`,
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({ session_id: sessionId }),
+						}
+					);
 
 					if (!response.ok) {
 						throw new Error(
@@ -40,11 +41,12 @@ export default function ManageLicense({ billing, setBilling, user }) {
 
 					const responseData = await response.json();
 					const fetchedSession = responseData.data;
+					console.log("Session data:", fetchedSession);
 
 					if (fetchedSession.status === "complete") {
-						setSubscription(fetchedSession);
+						await setSubscription(fetchedSession);
 					} else {
-						setErrorMessage("Subscription payment failed.");
+						setErrorMessage("error.license");
 					}
 				}
 			} catch (error) {
@@ -61,26 +63,30 @@ export default function ManageLicense({ billing, setBilling, user }) {
 		}
 
 		if (session.status === "complete") {
-			const { data, error } = await supabase.rpc(
-				"update_billing_yearly",
-				{
-					logged_user_id: user.id,
-					stripe_session_id: session.id,
-					stripe_customer_id: session.customer,
-				}
-			);
+			try {
+				const response = await fetch("/api/update-yearly", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						logged_user_id: user.id,
+						stripe_session_id: session.id,
+						stripe_customer_id: session.customer,
+					}),
+				});
 
-			if (error) {
-				console.error("Error updating billing subscription:", error);
-			} else {
-				console.log("Billing subscription updated successfully:", data);
-				//const fetchedBilling = await fetchBilling(user.id);
-				//setBilling(fetchedBilling);
-				setSuccessMessage("Subscription payment successful.");
-				router.push("/home/subscription");
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+
+				setSuccessMessage("success.license.purchase");
+				router.push("/home/license");
 				setTimeout(() => {
 					location.reload();
 				}, 1000);
+			} catch (error) {
+				console.error("Failed to update billing subscription: ", error);
 			}
 		}
 	};
@@ -89,20 +95,31 @@ export default function ManageLicense({ billing, setBilling, user }) {
 		if (!user) {
 			return;
 		}
-		const { data, error } = await supabase.rpc("update_billing_trial", {
-			logged_user_id: user.id,
-		});
 
-		if (error) {
-			console.error("Error updating billing trial:", error);
-		} else {
-			console.log("Billing trial updated successfully:", data);
-			//const fetchedBilling = await fetchBilling(user.id);
-			//setBilling(fetchedBilling);
-			setSuccessMessage("Free trial started successfully.");
+		try {
+			const response = await fetch("/api/update-trial", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					logged_user_id: user.id,
+					lang: user.language_id,
+					email: user.email,
+					name: user.name,
+				}),
+			});
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			setSuccessMessage("success.license.trial");
 			setTimeout(() => {
 				location.reload();
 			}, 1000);
+		} catch (error) {
+			console.error("Failed to update billing trial: ", error);
 		}
 	};
 
@@ -140,6 +157,135 @@ export default function ManageLicense({ billing, setBilling, user }) {
 		}
 	};
 
+	const renderActiveSubscriptionType = () => {
+		if (billing.status === "active") {
+			if (billing.subscription === "trial") {
+				return (
+					<span className="lowercase font-semibold underline underline-offset-4">
+						<T tkey="license.details.type.options.trial" />
+					</span>
+				);
+			} else if (billing.subscription === "yearly") {
+				return (
+					<span className="lowercase font-semibold underline underline-offset-4">
+						<T tkey="license.details.type.options.yearly" />
+					</span>
+				);
+			} else {
+				return (
+					<span className="lowercase font-semibold underline underline-offset-4">
+						<T tkey="license.details.type.options.lifetime" />
+					</span>
+				);
+			}
+		}
+	};
+
+	const isExpiringSoon = () => {
+		const today = new Date();
+		const expirationDate = new Date(billing.expiration_date);
+		const diffTime = Math.abs(expirationDate - today);
+		const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+		return diffDays <= 30;
+	};
+
+	const renderActiveSubscription = () => {
+		if (billing.status === "active") {
+			return (
+				<>
+					<p>
+						<T tkey="license.manage.active.a1" />{" "}
+						{renderActiveSubscriptionType()}{" "}
+						<T tkey="license.manage.active.a2" />{" "}
+						<span className="font-semibold uppercase text-accent_color px-1">
+							<T tkey="license.manage.active.a3" />
+						</span>
+						. <T tkey="license.manage.active.a4" />
+					</p>
+					{billing.subscription === "trial" ||
+					(billing.subscription === "yearly" && isExpiringSoon()) ? (
+						<div className="mt-2">
+							<p>
+								{billing.subscription === "trial" ? (
+									<T tkey="license.manage.purchase.p2" />
+								) : (
+									<T tkey="license.manage.purchase.p3" />
+								)}{" "}
+								<span className="font-semibold uppercase text-mid_blue px-1">
+									<T tkey="license.manage.purchase.p4" />
+								</span>{" "}
+								<T tkey="license.manage.purchase.p5" />.
+							</p>
+							<div className="flex justify-end">
+								<Button
+									variant="signup"
+									className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
+									onClick={handleCheckout}
+								>
+									<T tkey="license.manage.buttons.checkout" />
+								</Button>
+							</div>
+						</div>
+					) : null}
+				</>
+			);
+		}
+	};
+
+	const renderInactiveSubscription = () => {
+		if (billing.status === "inactive") {
+			return (
+				<>
+					<div>
+						<p>
+							<T tkey="license.manage.inactive.i1" />{" "}
+							<span className="font-semibold uppercase text-accent_delete px-1">
+								<T tkey="license.manage.inactive.i2" />
+							</span>
+							.
+						</p>
+					</div>
+					{billing.subscription === "none" && !billing.isTrialUsed ? (
+						<>
+							<p className="mt-2">
+								<T tkey="license.manage.trial.t1" />{" "}
+								<span className="font-semibold uppercase text-accent_color px-1">
+									<T tkey="license.manage.trial.t2" />
+								</span>{" "}
+								<T tkey="license.manage.trial.t3" />.
+							</p>
+							<div className="flex justify-end">
+								<Button
+									variant="login"
+									className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
+									onClick={handleNewTrial}
+								>
+									<T tkey="license.manage.buttons.trial" />
+								</Button>
+							</div>
+						</>
+					) : null}
+					<p>
+						<T tkey="license.manage.purchase.p1" />{" "}
+						<span className="font-semibold uppercase text-mid_blue px-1">
+							<T tkey="license.manage.purchase.p4" />
+						</span>{" "}
+						<T tkey="license.manage.purchase.p5" />.
+					</p>
+					<div className="flex justify-end">
+						<Button
+							variant="signup"
+							className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
+							onClick={handleCheckout}
+						>
+							<T tkey="license.manage.buttons.checkout" />
+						</Button>
+					</div>
+				</>
+			);
+		}
+	};
+
 	return (
 		<>
 			<ErrorMessage
@@ -151,69 +297,8 @@ export default function ManageLicense({ billing, setBilling, user }) {
 				setSuccessMessage={setSuccessMessage}
 			/>
 			<div className="text-sm m-2">
-				{billing.status === "active" ? (
-					<p>
-						Your subscription is{" "}
-						<span className="font-semibold uppercase text-accent_color px-1">
-							active
-						</span>
-						. Enjoy it!
-					</p>
-				) : billing.subscription === "none" && !billing.isTrialUsed ? (
-					<div>
-						<p>
-							You have a{" "}
-							<span className="font-semibold uppercase text-accent_color px-1">
-								free trial
-							</span>{" "}
-							available for 30 days.
-						</p>
-						<div className="flex justify-end">
-							<Button
-								variant="login"
-								className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
-								onClick={handleNewTrial}
-							>
-								Start free trial now
-							</Button>
-						</div>
-						<p>
-							You can purchase a{" "}
-							<span className="font-semibold uppercase text-mid_blue px-1">
-								yearly subscription
-							</span>{" "}
-							for 89 € per year.
-						</p>
-						<div className="flex justify-end">
-							<Button
-								variant="signup"
-								className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
-								onClick={handleCheckout}
-							>
-								Checkout yearly plan
-							</Button>
-						</div>
-					</div>
-				) : (
-					<div>
-						<p>
-							Your subscription is{" "}
-							<span className="font-semibold uppercase text-accent_delete px-1">
-								inactive
-							</span>
-							.
-						</p>
-						<div className="flex justify-end">
-							<Button
-								variant="signup"
-								className="my-2 w-full md:w-3/5 lg:w-2/5 xl:w-1/5 uppercase text-base"
-								onClick={handleCheckout}
-							>
-								Checkout yearly plan
-							</Button>
-						</div>
-					</div>
-				)}
+				{renderActiveSubscription()}
+				{renderInactiveSubscription()}
 			</div>
 		</>
 	);
